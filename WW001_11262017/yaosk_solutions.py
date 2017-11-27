83d90ad8a4b0ca5edc1e90dc6e5aa0c212ffe4d0
Kai Yao <kaiyaomail@gmail.com>
2017-11-26 17:24:07 -0800

    Add LeetCode#007, #009 solution in Python for WW001, from Shunkai

--- WW001_11262017/yaosk_solutions.py
+++ WW001_11262017/yaosk_solutions.py
@@ -36,6 +36,80 @@ class Solution:
             else:
                 sumdict[item] = index
 
+"""
+7. Reverse Integer
+Given a 32-bit signed integer, reverse digits of an integer.
+
+Example 1:
+
+Input: 123
+Output:  321
+Example 2:
+
+Input: -123
+Output: -321
+Example 3:
+
+Input: 120
+Output: 21
+Note:
+Assume we are dealing with an environment which could only hold integers within the 32-bit
+ signed integer range. For the purpose of this problem, assume that your function returns 
+ 0 when the reversed integer overflows.
+"""
+class Solution:
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        """
+        Another short and nice python solution:
+        s = cmp(x, 0) #signal indicator
+        r = int(`s*x`[::-1]) #revese string
+        return s*r * (r < 2**31) #get negtive signal
+        """
+        ret = temp = 0;
+        temp = abs(x);
+        while temp:
+            ret *= 10;
+            ret += temp % 10;
+            temp = int(temp/10);
+
+        if x < 0:
+            ret = -ret;
+        if ret > 2147483647 or ret < -2147483648:
+            ret = 0;
+        return ret;
+
+"""
+9. Palindrome Number
+Determine whether an integer is a palindrome. Do this without extra space.
+Some hints:
+Could negative integers be palindromes? (ie, -1)
+
+If you are thinking of converting the integer to string, note the restriction of using extra space.
+
+You could also try reversing an integer. However, if you have solved the problem "Reverse Integer", you know that the reversed integer might overflow. How would you handle such case?
+
+There is a more generic way of solving this problem.
+"""
+class Solution:
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+            return False
+
+        reversex = 0
+        temp = x
+        while temp:
+            reversex *= 10
+            reversex += temp%10
+            temp = int(temp/10)
+        return reversex == x
 
 """
 2. Add Two Numbers
