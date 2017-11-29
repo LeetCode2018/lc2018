681b11c278cab7aedd54da42c057480993b10c70
Kai Yao <kaiyaomail@gmail.com>
2017-11-29 00:14:18 -0800

    Add LeetCode#004 solution in Python for WW002, from Shunkai

--- WW002_12032017/yaosk_solution_ww002.py
+++ WW002_12032017/yaosk_solution_ww002.py
@@ -15,7 +15,40 @@ nums2 = [3, 4]
 
 The median is (2 + 3)/2 = 2.5
 """
+class Solution:
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        #The median is either n//2 for odd, or (n//2+n//2-1)/2 for even
+        lenA, lenB = len(nums1), len(nums2) #n=lenA+lenB
+        if lenA==0 and lenB==0:
+            return None
+        i = j = curr = 0
+        mid1 = mid2 = 0 #mid1 for the n//2-1 number, mid2 for the n//2 number
+        while (i+j < lenA+lenB):
+            if i >= lenA or (j < lenB and nums1[i] >= nums2[j]):
+                curr = nums2[j]
+                j += 1
+            elif j >= lenB or (i < lenA and nums1[i] < nums2[j]):
+                curr = nums1[i]
+                i += 1
+            else:
+                print("ERROR!")
+                return None
 
+            if i+j-1 == (lenA+lenB)//2-1:
+                mid1 = curr
+            if i+j-1 == (lenA+lenB)//2:
+                mid2 = curr
+                break;
+
+        if (lenA+lenB) & 1:#odd
+            return mid2
+        else:#even
+            return (mid1+mid2)/2
 
 """
 13. Roman to Integer - Easy
