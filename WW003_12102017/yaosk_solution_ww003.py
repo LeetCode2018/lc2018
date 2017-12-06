46d1a7698bdcb466deb8a21608d9c17fdc57cebd
Kai Yao <kaiyaomail@gmail.com>
2017-12-06 00:29:47 -0800

    Add LeetCode#016, #23 solution in Python for WW003, from Shunkai TODO: "key=lambda" usage

--- WW003_12102017/yaosk_solution_ww003.py
+++ WW003_12102017/yaosk_solution_ww003.py
@@ -78,3 +78,40 @@ class Solution:
                     while l < r and nums[r] == nums[r-1] < r: r -= 1
                     l, r = l+1, r-1
         return ret
+
+"""
+16. 3Sum Closest
+Given an array S of n integers, find three integers in S such that the sum is closest to a given number, target. Return the sum of the three integers. You may assume that each input would have exactly one solution.
+
+    For example, given array S = {-1 2 1 -4}, and target = 1.
+
+    The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+"""
+class Solution:
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        closestSum = 0
+        currSum, currGap, minGap = 0, 0, sys.maxsize
+        if not nums or len(nums) < 3:
+            return 0
+        nums.sort()
+        for i in range(len(nums)-2):
+            l, r = i + 1, len(nums)-1
+            while l < r:
+                currSum = nums[i]+nums[l]+nums[r]
+                currGap = abs(currSum-target)
+                if currGap < minGap:
+                    minGap = currGap
+                    closestSum = currSum
+                if currSum > target:
+                    r -= 1
+                elif currSum < target:
+                    l += 1
+                else: #currSum == target
+                    return target
+        return closestSum
+            
