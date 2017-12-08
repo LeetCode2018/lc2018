7e96a0b9e9c0e0a629f1ccba535098e812717b81
Kai Yao <kaiyaomail@gmail.com>
2017-12-08 00:38:41 -0800

    Add LeetCode#017, #018 solution in Python for WW003, from Shunkai

--- WW003_12102017/yaosk_solution_ww003.py
+++ WW003_12102017/yaosk_solution_ww003.py
@@ -115,6 +115,78 @@ class Solution:
                     return target
         return closestSum
 
+"""
+17. Letter Combinations of a Phone Number
+Given a digit string, return all possible letter combinations that the number could represent.
+
+Input:Digit string "23"
+Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+"""
+class Solution:
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        digitMap = { '1': '',    '2': 'abc', '3': 'def',
+                     '4': 'ghi', '5': 'jkl', '6': 'mno',
+                     '7': 'pqrs','8': 'tuv', '9': 'wxyz',
+                     '0': ' '}
+        ret = [''] if digits else []
+        for d in digits:
+            ret = [item+append for append in digitMap[d] for item in ret]
+        
+        return ret
+"""
+18. 4Sum
+Given an array S of n integers, are there elements a, b, c, and d in S such that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.
+
+Note: The solution set must not contain duplicate quadruplets.
+
+For example, given array S = [1, 0, -1, 0, -2, 2], and target = 0.
+
+A solution set is:
+[
+  [-1,  0, 0, 1],
+  [-2, -1, 1, 2],
+  [-2,  0, 0, 2]
+]
+"""
+class Solution:
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        if len(nums) < 4:
+            return []
+        Ret = []
+        self.nSum(nums, target, 4, [], Ret)
+        return Ret
+
+    def nSum(self, nums, target, n, lastRet, Ret):
+        if n > len(nums) or n < 2:
+            return
+        if n == 2:
+            l,r = 0,len(nums)-1
+            while l < r:
+                s = nums[l] + nums[r]
+                if s == target:
+                    Ret.append(lastRet + [nums[l], nums[r]])
+                    l += 1
+                    while nums[l] == nums[l-1] and l < r:
+                        l += 1
+                elif s > target: r -= 1
+                else: l += 1
+        else:
+            for i in range(0, len(nums)-n+1):
+                if i > 0 and nums[i] == nums[i-1]:
+                    continue
+                else:
+                    self.nSum(nums[i+1:], target-nums[i], n-1, lastRet+[nums[i]], Ret)
+        return
 
 """
 23. Merge k Sorted Lists
